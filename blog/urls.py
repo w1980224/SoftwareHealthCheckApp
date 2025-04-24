1b1ec13fd2c81f7dbc0d375e6c6bb1b411b556e5
w1980224 <w1980224@westminster.ac.uk>
2025-04-24 16:29:33 +0100

    commit

--- blog/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.voting_page, name='voting_page'),
-]
--- blog/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts render
-
-def voting_page(request):
-    return render(request, 'blog/voting.html')
