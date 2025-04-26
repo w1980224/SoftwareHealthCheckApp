8d6bdb2b372262ee0b535d81f5becc087ab046d8
w1980224 <w1980224@westminster.ac.uk>
2025-04-26 12:29:19 +0100

    commit

--- app/models.py
+++ app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Vote(models.Model):
+    option_name = models.CharField(max_length=100)
+    votes = models.IntegerField(default=0)
+
+    def _str_(Self):
+        return self.option_name
--- app/views.py
+++ app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponseReDirect
+from .models import Vote
 
-# Create your views here.
+def vote(request):
+    if request.method == "POST":
+        selected_option = request.POST.get('vote_option')
+
+        if selected_option:
+        vote_option = Vote.objects.get(id=selected_option)
+        vote_option.votes += 1
+        vote_option.save()
+        return redirect('/')
+
+    return HttpResponseRedirect('/request/')
+
+    options = Vote.objects.all()
+
+    return render(request, 'voting.html', {'options': options})
+
