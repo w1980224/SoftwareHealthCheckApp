448b457ddf91b591cff4e69b0926430fc1cfd3c1
Mfaizah <165774265+Mfaizah@users.noreply.github.com>
2025-05-01 14:28:18 +0100

    commit

--- coursework/page/urls.py
+++ coursework/page/urls.py
@@ -3,14 +3,14 @@ from . import views
 
 urlpatterns = [
    
-    path('begin-health-check/', views.begin_health_check, name='begin_health_check'),
-    path('view-summary/', views.view_summary, name='view_summary'),
-    path('view-progress/', views.view_progress, name='view_progress'),
-    path('view-cards/', views.view_cards, name='view_cards'),
+    path('begin_health_check/', views.begin_health_check, name='begin_health_check'),
+    path('view_summary/', views.view_summary, name='view_summary'),
+    path('view_progress/', views.view_progress, name='view_progress'),
+    path('view_cards/', views.view_cards, name='view_cards'),
 
     # Optional direct access
     path('engineer/', views.engineer_landing, name='engineer'),
-    path('team-leader/', views.team_leader_landing, name='team_leader'),
-    path('department-leader/', views.department_leader_landing, name='department_leader'),
-    path('senior-manager/', views.senior_manager_landing, name='senior_manager'),
+    path('team_leader/', views.team_leader_landing, name='team_leader'),
+    path('department_leader/', views.department_leader_landing, name='department_leader'),
+    path('senior_manager/', views.senior_manager_landing, name='senior_manager'),
 ]
