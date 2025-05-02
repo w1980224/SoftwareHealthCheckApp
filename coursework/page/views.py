8bc90477679adcfacc5f60b3e6e0b6f20e2587db
Mfaizah <165774265+Mfaizah@users.noreply.github.com>
2025-05-02 15:44:03 +0100

    commit

--- coursework/page/views.py
+++ coursework/page/views.py
@@ -2,28 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Profile
 
-# Main landing page
 
-#def landing(request):
-
-   # if request.user.is_authenticated:
-        #try:
-           # profile = Profile.objects.get(user=request.user)
-           # if profile.role == 'engineer':
-            #    return render(request, 'page/engineer.html', {'profile': profile})
-           # elif profile.role == 'team_leader':
-            #    return render(request, 'page/team_leader.html', {'profile': profile})
-           # elif profile.role == 'department_leader':
-            #    return render(request, 'page/department_leader.html', {'profile': profile})
-           # elif profile.role == 'senior_manager':
-            #    return render(request, 'page/senior_manager.html', {'profile': profile})
-       
-    # except Profile.DoesNotExist:
-         #   return render(request, "page/landing.html")  # fallback page
-   # else:
-       # return render(request, "page/landing.html")
-
-# Stub views for feature pages
 def begin_health_check(request):
     return HttpResponse("Begin Health Check page (to be implemented)")
 
