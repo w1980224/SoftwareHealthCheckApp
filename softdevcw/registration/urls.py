a9af03837e842f1daeca6db1020dfaa1b2006928
w1980224 <w1980224@westminster.ac.uk>
2025-05-03 01:37:45 +0100

    commit

--- softdevcw/registration/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import home, signup, login, logout
-
-urlpatterns = [
-    path('', home, name='home'),
-    path('signup/', signup, name='signup'),
-    path('login/', login, name="login"),
-    path('logout/', logout, name="logout"),
-]
